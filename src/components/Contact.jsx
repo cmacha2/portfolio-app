f641ddc5c48ad99ebd24faf588f6bf92c1cf3894
cmacha2 <cristiancmg127@gmail.com>
2022-08-03 08:30:34 -0700

    add info contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,81 @@
-import { Box, styled } from '@mui/material'
+import { Box, Button, IconButton, styled, Typography } from '@mui/material'
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import React from 'react'
 
+const Wrapper = styled((Box))(({theme})=>({
+    display: "flex",
+    justifyContent:"center",
+    alignItems:"center",
+    flexDirection: "column",
+}))
+
 const Container = styled(Box)(({ theme }) => ({
-    height: "100vh",
+     height: "85vh",
+    // backgroundColor: "blue",
+     display: "flex",
+    justifyContent:"center",
+    alignItems:"center",
+    flexDirection: "column",
+}))
+
+const ContainerContact = styled(Box)(({ theme }) => ({ 
+    height: "50vh",
+    width: '80vh',
+    // backgroundColor:'green',
+    display: 'flex',
+    flexDirection:"column",
+    gap:"20px",
+    justifyContent:"center",
+    alignItems:"center",
+    color:"#fff"
 }))
 
 const Footer = styled(Box)(({ theme }) => ({
-    
+    // backgroundColor: "red",
+    height: "15vh",
+    width: "90%",
+    borderTop: "1px solid #fff",
+    color: "#fff",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
 }))
+
+const Icons = styled(Box)(({theme}) => ({
+    height: '20%'
+}))
+
+const ButtonIcon = styled(IconButton)(({ theme }) => ({
+    backgroundColor: "transparent",
+    // color: "#fff",
+    width: "60px",
+    height: "60px",
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  }));
 
 const Contact = () => {
   return (
+    <Wrapper>
     <Container >
-        <Footer>
-
-        </Footer>
+        <ContainerContact>
+            <Typography variant="h6">Would you like to have my contact?</Typography>
+            <Icons>
+            <ButtonIcon href="https://www.linkedin.com/in/cmacha2/" target="_blank" size="large" ><LinkedInIcon fontSize='large'/></ButtonIcon>
+          <ButtonIcon  href="https://github.com/cmacha2" target="_blank"  size="large"><GitHubIcon fontSize='large'/></ButtonIcon>
+          <ButtonIcon  href="https://twitter.com/cmacha2_08" target="_blank"  size="large" ><TwitterIcon fontSize='large'/></ButtonIcon>
+            </Icons>
+            <Typography variant="h6" >Do you have a project in mind? Let's make it happen!</Typography>
+            <Typography variant='h6' color="#0ee687">cmacha2.08@gmail.com</Typography>
+        </ContainerContact>
     </Container>
+    <Footer>
+        <Typography variant="p">© 2022 Cristian Machado</Typography>
+    </Footer>
+   </Wrapper>
   )
 }
 
